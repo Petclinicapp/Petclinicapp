7910f7701ca5f8b6744952135b11677c444ab71a
eglereact <eglereactjs@gmail.com>
2025-02-04 13:28:17 +0200

    home page

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Banner from "../components/Banner";
+import Sections from "../components/Sections";
+
+function HomePage() {
+  return (
+    <div>
+      <Banner />
+      <Sections />
+    </div>
+  );
+}
+export default HomePage;
