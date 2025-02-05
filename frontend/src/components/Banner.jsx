ccff66be3668a9096b111615a49881ca5724ea20
eglereact <eglereactjs@gmail.com>
2025-02-05 10:05:19 +0200

    home page footer

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -10,9 +10,9 @@ function Banner() {
       }}
     >
       <Navbar />
-      <div className="absolute inset-0 flex justify-center mt-16 lg:ml-44 items-left text-white flex-col">
-        <div className=" p-10 rounded-lg w-1/3">
-          <h1 className="text-5xl">Welcome to Our Vet Clinic!</h1>
+      <div className="absolute inset-0 flex justify-center mt-16 lg:ml-34 items-left text-white flex-col">
+        <div className=" p-10 rounded-lg w-full md:w-2/3 lg:w-1/2">
+          <h1 className="text-3xl lg:text-5xl">Welcome to Our Vet Clinic!</h1>
           <p className=" mt-4 text-2xl">
             At our clinic, we provide high-quality veterinary care to ensure
             your pets stay healthy and happy. Our team of experienced
@@ -22,7 +22,7 @@ function Banner() {
           </p>
           <Link
             to="/signin"
-            className="text-white uppercase font-bold justify-center flex w-32 bg-[#3977A4] text-lg mt-6 py-2 rounded transition-colors duration-300 hover:bg-[#2c6181]"
+            className="text-white uppercase font-bold justify-center flex w-32 bg-[#016891] text-lg mt-6 py-2 rounded transition-colors duration-300 hover:bg-[#2c6181]"
           >
             Sign In
           </Link>
--- frontend/src/components/Sections.jsx
+++ frontend/src/components/Sections.jsx
@@ -3,15 +3,15 @@ import { homeInfo } from "../data";
 
 function Sections() {
   return (
-    <div className="flex w-full mt-10">
+    <div className="flex flex-col lg:flex-row w-full mt-10">
       {homeInfo.map((card) => (
         <Link
           key={card.id}
           to={card.link}
           style={{ backgroundColor: card.color }}
-          className="flex w-full flex-col p-20 text-center gap-4"
+          className="flex items-center w-full flex-col p-20 text-center gap-4"
         >
-          <img src={card.img} />
+          <img src={card.img} className="w-2/3" />
           <h1 className="text-4xl uppercase text-white">{card.label}</h1>
           <p className="text-white text-xl">{card.info}</p>
         </Link>
