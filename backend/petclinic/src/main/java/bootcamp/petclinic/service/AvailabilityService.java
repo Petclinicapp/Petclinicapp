487fe01d871b463c403253165bdf182d099122f8
ZeyDy <zydrunasdauksas302pk@gmail.com>
2025-02-12 12:28:42 +0200

    updated AvailabilityService

--- backend/petclinic/src/main/java/bootcamp/petclinic/service/AvailabilityService.java
+++ backend/petclinic/src/main/java/bootcamp/petclinic/service/AvailabilityService.java
@@ -2,18 +2,22 @@ package bootcamp.petclinic.service;
 
 import bootcamp.petclinic.model.Availability;
 import bootcamp.petclinic.model.DoctorSchedule;
+import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AvailabilityService {
 
     public Optional<Availability> findAvailableSlot(String doctorId, LocalDate date, LocalTime time, List<DoctorSchedule> schedules) {
         return schedules.stream()
                 .filter(schedule -> schedule.getDoctorId().equals(doctorId))
                 .flatMap(schedule -> schedule.getAvailableSlots().stream())
-                .filter(slot -> slot.getAvailableDate().equals(date) && slot.getAvailableTime().equals(time) && !slot.isBooked())
+                .filter(slot -> slot.getAvailableDate().equals(date)
+                        && slot.getAvailableTime().equals(time)
+                        && !slot.isBooked())
                 .findFirst();
     }
 
